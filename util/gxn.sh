30a0c711ecc2293a68726c696aae68ebc4bb94ad
Soojin Nam <jsunam@gmail.com>
2018-12-30 16:17:32 +0900

    Update gxn.sh

--- util/gxn.sh
+++ util/gxn.sh
@@ -7,29 +7,21 @@ ERROR=0
 case $2 in
     mplibcode)
         $5 $3
-        rm -rf *.mp *.mpx
         ERROR=$?
         ;;
     tikzpicture)
         $5 $3
         pdf2svg $3.pdf $3.svg
-        rm -rf *.tex *.aux *.pdf
         ERROR=$?
         ;;
     graphviz)
         $5 -Tsvg $3.gv -o $3.$4
-        rm -rf *.gv
-        ERROR=$?
-        ;;
-    sudoku)
-        $5 $3
-        rm -rf *.dlx
         ERROR=$?
         ;;
     *)
         echo 'NOT SUPPORTED'
 esac
 
-rm -rf *.log
+rm -rf *.log *.mp *.mpx *.tex *.aux *.pdf *.gv
 
 exit $ERROR
