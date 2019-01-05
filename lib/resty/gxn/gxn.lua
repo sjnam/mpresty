d6575a696e628aea3f567b729b4322f15a6ca7e4
sjnam <jsunam@gmail.com>
2019-01-05 16:19:12 +0900

    s

--- lib/resty/gxn/gxn.lua
+++ lib/resty/gxn/gxn.lua
@@ -9,10 +9,17 @@ local gxn = require("resty.gxn.base"):new {
    tag_name = "gxn",
 }
 
+local graphics = {
+   mp = "mplibcode",
+   tikz = "tikzpicture",
+   dot = "digraph",
+   neato = "neatograph"
+}
 
-local function set_property (self, src)
+
+local function set_graphics (self, src, cmd)
    local _, _, ext = str_find(src, "%.(%a+)$")
-   local gx = require("resty.gxn")[ext]
+   local gx = require("resty.gxn")[graphics[cmd or ext]]
    self.cmd = gx.cmd
    self.ext = gx.ext
    self.tag_name = gx.tag_name
@@ -23,7 +30,7 @@ end
 
 gxn.getContent = function (self, node)
    local src = node:getAttribute("src");
-   set_property(self, src)
+   set_graphics(self, src, node:getAttribute("cmd"))
    local f = fopen(ngx_var.document_root.."/"..src, "r");
    local content =  f:read("*a")
    f:close()
