f5bd4bc62566c4730e9304762e92bffbb0f8bc4f
donald-knuth <donald.knuth@kakaocorp.com>
2019-01-08 10:11:49 +0900

    error process

--- lib/resty/gxn.lua
+++ lib/resty/gxn.lua
@@ -5,7 +5,10 @@ local ipairs = ipairs
 local fopen = io.open
 local str_gsub = string.gsub
 local ngx_var = ngx.var
-local ngx_print = ngx.print
+local ngx_say = ngx.say
+local ngx_log = ngx.log
+local ngx_ERR = ngx.ERR
+local ngx_exit = ngx.exit
 local gumbo_parse = require("gumbo").parse
 
 
@@ -14,7 +17,7 @@ local _M = {
 }
 
 
-local GXS = {
+local graphics = {
    "mplibcode",
    "tikzpicture",
    "digraph",
@@ -22,28 +25,33 @@ local GXS = {
 }
 
 
-for _, v in ipairs(GXS) do
+for _, v in ipairs(graphics) do
    _M[v] = require("resty.gxn."..v)
 end
 
 
 function _M:render (fn_update_node)
    local f = fopen(ngx_var.document_root..ngx_var.uri, "r")
+   if not f then
+      ngx_exit(404)
+   end
+
    local content = f:read("*a")
    f:close()
 
-   for _, v in ipairs(GXS) do
+   for _, v in ipairs(graphics) do
       content = str_gsub(content, "(<"..v.."%s+.-src%s*=.-)/?>", "%1></"..v..">")
    end
 
    local doc = gumbo_parse(content)
    if not doc then
-      ngx.exit(404)
+      ngx_log(ngx_ERR, "fail to parse html")
+      ngx_exit(500)
    end
-   for _, v in ipairs(GXS) do
+   for _, v in ipairs(graphics) do
       doc = self[v]:setDocument(doc):updateDocument(fn_update_node)
    end
-   ngx_print(doc:serialize())
+   ngx_say(doc:serialize())
 end
 
 
