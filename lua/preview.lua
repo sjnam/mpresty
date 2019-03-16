3aac7550da2ae9992041cf8e6ed04b158e9232fb
sjnam <jsunam@gmail.com>
2019-03-16 21:08:02 +0900

    content_by_lua_block

--- lua/preview.lua
+++
@@ -1,7 +0,0 @@
-local gxn = require "gxn"
-
-ngx.req.read_body()
-local args = ngx.req.get_post_args()
-local html = string.format("<%s cmd='%s' width='400' cache='no'>%s</%s>",
-                           args.gx, args.cmd or "", args.code, args.gx)
-gxn:preview(html)
