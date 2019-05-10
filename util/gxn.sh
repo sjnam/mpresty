40bdd3c26719f55000fd536aedd2f145c732dfd1
sjnam <jsunam@gmail.com>
2019-05-11 05:38:25 +0900

    remove auxs

--- util/gxn.sh
+++ util/gxn.sh
@@ -8,20 +8,21 @@ case $2 in
     mplibcode)
         $6 $3
         ERROR=$?
+        rm -rf $3.mp* $3.log
         ;;
     graphviz)
         $6 -T$5 $3.$4 -o $3.$5
         ERROR=$?
+        rm -rf $3.gv
         ;;
     tikzpicture)
         $6 $3
         pdf2svg $3.pdf $3.$5
         ERROR=$?
+        rm -rf $3.tex $3.aux $3.pdf $3.log
         ;;
     *)
         echo 'NOT SUPPORTED'
 esac
-
-rm -rf $3.mp $3.mpx $3.gv $3.tex $3.aux $3.pdf $3.log
 
 exit $ERROR
