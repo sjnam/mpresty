e52385a10902736942ec23a52c9c2f81f53fe057
sjnam <jsunam@gmail.com>
2020-12-09 20:18:27 +0900

    rf

--- mpresty.sh
+++ mpresty.sh
@@ -8,18 +8,18 @@ case $2 in
     mplibcode)
         $6 $3
         ERROR=$?
-        rm -rf $3.mp* $3.log
+        rm $3.mp* $3.log
         ;;
     graphviz)
         $6 -T$5 $3.$4 -o $3.$5
         ERROR=$?
-        rm -rf $3.gv
+        rm $3.gv
         ;;
     tikzpicture)
         $6 $3
         pdf2svg $3.pdf $3.$5
         ERROR=$?
-        rm -rf $3.tex $3.aux $3.pdf $3.log
+        rm $3.tex $3.aux $3.pdf $3.log
         ;;
     *)
         echo 'NOT SUPPORTED'
