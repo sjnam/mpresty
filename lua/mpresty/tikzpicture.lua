7570ad9e3ffa0850a39c5f901606122a8e8f1785
sjnam <jsunam@gmail.com>
2020-12-10 19:18:43 +0900

    rm comment

--- lua/mpresty/tikzpicture.lua
+++ lua/mpresty/tikzpicture.lua
@@ -5,7 +5,6 @@ return require("mpresty.base"):new {
    preamble = [[
                  \documentclass[tikz]{standalone}
                  \usepackage{pgfplots}
-                 %\usepackage{fourier}
                  \usepackage{xifthen}
                  \usetikzlibrary{arrows, backgrounds, calc, intersections}
                  \usetikzlibrary{matrix, mindmap, positioning, shapes}
