8177c58163e8f6b20222d63e3b28d18405aea895
sjnam <jsunam@gmail.com>
2020-12-19 14:21:22 +0900

    preview

--- lua/preview.lua
+++ lua/preview.lua
@@ -5,14 +5,14 @@ local mpresty = require "mpresty"
 ngx.req.read_body()
 
 local args = ngx.req.get_post_args()
-
 local cmd = ''
 if args.cmd and args.cmd ~= '' then
    cmd = " cmd='" .. args.cmd .. "'"
 end
 
 local html = table.concat {
-   "<", args.gx, cmd, " cache='no', width='400'>", args.code, "</", args.gx, ">"
+   "<", args.gx, cmd, " cache='no', width='400'>\n", args.code,
+   "\n</", args.gx, ">"
 }
 
 local gx = mpresty.new { doc = gumbo.parse(html) }
